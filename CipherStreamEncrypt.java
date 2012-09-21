f5b27e7dfd9e7f993ca24dca6b06cb06b920439f
Ivan Baguio <baguio.ivan@gmail.com>
2012-09-21 17:18:53 +0800

    Buggy version of CipherStreamEncrypt.java

--- CipherStreamEncrypt.java
+++ CipherStreamEncrypt.java
@@ -3,6 +3,7 @@ import java.security.*;
 import javax.crypto.*;
 import javax.crypto.spec.*;
 import java.security.spec.*;
+import java.util.*;
 
 public class CipherStreamEncrypt{
     public static void main(String args[]){
@@ -26,16 +27,27 @@ public class CipherStreamEncrypt{
                 System.out.println("Encrypting "+filename);
                 System.out.println("Password "+password);
                 System.out.println("Size: "+new File(filename).length()+" bytes");
-                encrypt(getKey(password.toCharArray()),filename);
+                byte[] iv = generateIV(password);
+                encrypt(getKey(password.toCharArray()),filename,iv);
             }else{
                 System.out.println("Decrypting "+filename);
                 System.out.println("Password "+password);
                 System.out.println("Size: "+new File(filename).length()+" bytes");
-                decrypt(getKey(password.toCharArray()),filename);
+                byte[] iv = generateIV(password);
+                decrypt(getKey(password.toCharArray()),filename,iv);
             }
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
+
+    /* hash the password and get the first 16 chars*/
+    private static byte[] generateIV(String password) throws Exception{
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes("UTF-8"));
+        byte[] hash = md.digest();
+        System.out.println("MD5 Hash: "+new String(hash,"UTF-8"));
+        return Arrays.copyOfRange(hash, 0,16);
     }
 
     private static SecretKey getKey(char[] password) throws Exception{
@@ -48,9 +60,10 @@ public class CipherStreamEncrypt{
         return secret;
     }
 
-    private static void encrypt(SecretKey key, String filename) throws Exception{
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE,key);
+    private static void encrypt(SecretKey key, String filename,byte[] iv) throws Exception{        
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE,key,new IvParameterSpec(iv));
+        System.out.println("IV: "+new String(iv,"UTF-8")+"len: "+iv.length);
         CipherInputStream cin = new CipherInputStream(new FileInputStream(filename),cipher);
         FileOutputStream fos = new FileOutputStream(new File("encxx_"+filename));
         int i;
@@ -59,12 +72,21 @@ public class CipherStreamEncrypt{
             fos.write(i);
             size+=1;
         }
-        System.out.println("Wrote "+size+" bytes"); 
+        System.out.println("Wrote "+size+" bytes to encxx_"+filename);
     }
 
-    private static void decrypt(SecretKey key, String filename) throws Exception{
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, key);
+    private static void printToFile(String filename,byte[] data) throws Exception{
+        FileOutputStream fos = new FileOutputStream(new File(filename));
+        DataOutputStream dos = new DataOutputStream(fos);
+        dos.write(data);
+        dos.close();
+        System.out.println("Wrote "+data.length+" bytes to "+filename);
+    }
+
+    private static void decrypt(SecretKey key, String filename,byte[] iv) throws Exception{
+        System.out.println("IV: "+new String(iv,"UTF-8")+"*");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
         FileInputStream fis =null;
         try{
             fis = new FileInputStream(filename);
@@ -92,5 +114,22 @@ public class CipherStreamEncrypt{
             e.printStackTrace();
         }
         return input;
+    }
+
+    private static byte[] getFileBytes(String filename, int size){
+        byte[] out = new byte[size];//64 bytes
+        try{
+            FileInputStream fis = new FileInputStream(filename);
+            DataInputStream dis = new DataInputStream(fis);
+            BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+            int c;
+            for (int i=0; (c = br.read()) != -1 ;i++) 
+                 out[i] = (byte)c;
+            System.out.println("Read "+out.length+" bytes");
+        }catch(Exception e){
+            System.out.println("Error fetching file");
+            return null;
+        }
+        return out;
     }
 }
